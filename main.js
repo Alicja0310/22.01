87ec83cfdd0fa9bdc43e2e70e1d8b61a5497d530
Alicja Spiewak <aliszjaspiewak@gmail.com>
2021-01-22 16:15:10 +0100

    js

---
+++ main.js
@@ -0,0 +1,32 @@
+const nimString = "10"
+const num = 10
+const num0 = 0
+const num1 = 1
+const num2 = 2
+const prawda = true // to 1 jako liczba
+const falsz = false // to 0 jako liczba
+
+console.log(num > num1)
+console.log(num0 + num1)
+console.log(num < num1)
+console.log(num0 > prawda)
+console.log(num1 * num2)
+console.log(num / num2)
+
+
+const arr = ["Janusz", 55, true, num]
+console.log(arr[2])
+
+const obj = {
+    name: "Janusz",
+    age: 13,
+    adress: "Nowa 17"
+}
+console.log(obj.age)
+
+
+const respons = [
+    { name: "Dawid", age: 23 },
+    { name: "Jakub", age: 17 }
+]
+console.log(respons[1].name)
