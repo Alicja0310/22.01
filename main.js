e6e835f1c4cf835d77d1f0941a4db661104a04a0
Alicja Spiewak <aliszjaspiewak@gmail.com>
2021-01-23 16:17:28 +0100

    mjs

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const nimString = "10"
+const numString = "10"
 const num = 10
 const num0 = 0
 const num1 = 1
 const num2 = 2
-const prawda = true // to 1 jako liczba
-const falsz = false // to 0 jako liczba
+const num3 = 3
+const prawda = true   // to 1 jako liczba
+const falsz = false   // to 0 jako liczba
 
 console.log(num > num1)
 console.log(num0 + num1)
@@ -12,6 +13,7 @@ console.log(num < num1)
 console.log(num0 > prawda)
 console.log(num1 * num2)
 console.log(num / num2)
+console.log(num < num3)
 
 
 const arr = ["Janusz", 55, true, num]
